6c8dd7a7c4d0975eec767b166bc07b242679a5a7
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2025-04-21 10:58:46 +0100

    Create locationlisting-fancybox.js

---
+++ locationlisting-fancybox.js
@@ -0,0 +1,7 @@
+Fancybox.bind("[data-fancybox]", {
+  // Optional: Add any custom options here
+  // For example:
+  animated: true,
+  showClass: "fancybox-zoomIn",
+  hideClass: "fancybox-zoomOut",
+});
