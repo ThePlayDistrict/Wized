db6785bd57b2c5b6c7958ded28514123b523acb7
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2025-05-07 16:09:16 +0100

    Delete locationlisting-fancybox.js

--- locationlisting-fancybox.js
+++
@@ -1,7 +0,0 @@
-Fancybox.bind("[data-fancybox]", {
-  // Optional: Add any custom options here
-  // For example:
-  animated: true,
-  showClass: "fancybox-zoomIn",
-  hideClass: "fancybox-zoomOut",
-});
